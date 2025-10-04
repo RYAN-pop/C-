5d58a14ee712441d70f61e2fb8137891c5c30001
RYAN-pop <zxc.ryan88@gmail.com>
2025-10-04 09:35:19 +0000

    from virtualBox

---
+++ apcs.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+void swapValue(int& a,int& b)
+{
+	int x=a;a=b;b=x;
+}
+
+
+int main()
+{
+	int num;
+	cout<<"students numbers:";
+	cin>>num;
+	cout<<"their grades";
+	int grades[num];
+	for(int i=0;i<num;i++)
+		cin>>grades[i];
+	for(int i=0;i<num-1;i++)
+	{
+		
+		if(grades[i]>grades[i+1])
+		{
+			//int x=grades[i];
+			//grades[i]=grades[i+1];
+			//grades[i+1]=x;
+			swapValue(grades[i],grades[i+1]);
+			i=0;
+		}
+	}
+	for(int i=0;i<num;i++)
+		cout<<grades[i]<<" "<<endl;
+	
+	
+	if(grades[0]>=60)
+		cout<<"all pass"<<endl;
+	else if(grades[num]<=60)
+		cout<<"all fail";
+		
+		
+	else{
+		int y;
+		for(int i=0;i<num;i++){
+			if(grades[i]<60)
+				y=grades[i];
+				
+			else{
+				cout<<"the lowest grade that pass:"<<grades[i]<<endl;
+				break;
+			}
+		}
+		cout<<"the highest grade that fail:"<<y<<endl;
+	}
+	
+return 0;
+}
